fa77e43fe88c89071f0da12eef6331450cb450c3
VillarealCarolV <carolvillareal16@gmail.com>
2024-08-24 22:52:20 +0800

    First commit

---
+++ src/components/User/User.js
@@ -0,0 +1,11 @@
+
+function User() {
+    return (
+        <div><h1>Carol Villareal</h1>
+            <h2>3B</h2>
+            <p>I lived in Bocaue Bulacan</p></div>
+
+    )
+}
+
+export default User;
